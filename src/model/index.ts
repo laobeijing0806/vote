e9c50d83a5ca53e609daae692d45c32b817b0638
liubojun <1570680014@qq.com>
2017-09-21 17:10:31 +0800

    update npm script

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,10 +2,7 @@ import * as mongoose from 'mongoose'
 import { url } from '../config'
 
 mongoose.connect(url, err => {
-  if (err) {
-    console.error('connect to %s error', url, err)
-    process.exit(1)
-  }
+  err && console.error('connect to %s error', url, err)
 })
 
 export { User } from './user'
